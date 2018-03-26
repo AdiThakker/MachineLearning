c78fef305ca34037ccfc0b47f8daf9fac2148564
Aditya D Thakker <aditya.thakker@live.com>
2018-03-26 19:39:44 -0400

    Initial Commit

---
+++ ScrapStationConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var module = ScrapPrediction.IsScrap(new Double[] { 1.0, 1.276541629, 15.88239006 });
+            Console.WriteLine($"This should not be Scrap -> Probability :{module.Item1:N2}  Scrap : {module.Item2}");
+
+            module = ScrapPrediction.IsScrap(new Double[] { 1.0, 2.815402647, 8.265707992 });
+            Console.WriteLine($"This should be Scrap -> Probability :{module.Item1:N2}  Scrap : {module.Item2}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
